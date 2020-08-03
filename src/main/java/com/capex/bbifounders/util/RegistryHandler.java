062ac0bcabe1b5f84ef88bc9891d0ca0d51ea309
Tommy <juhltommy@gmail.com>
2020-08-02 20:02:13 -0700

    Added Bigboyium Ore to the game!

--- src/main/java/com/capex/bbifounders/util/RegistryHandler.java
+++ src/main/java/com/capex/bbifounders/util/RegistryHandler.java
@@ -1,7 +1,10 @@
 package com.capex.bbifounders.util;
 
 import com.capex.bbifounders.TheFounders;
+import com.capex.bbifounders.blocks.BlockItemBase;
 import com.capex.bbifounders.items.ItemBase;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -12,12 +15,21 @@ public class RegistryHandler {
 
 
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TheFounders.MOD_ID);
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TheFounders.MOD_ID);
 
     public static void init () {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     // ITEMS
     public static final RegistryObject<Item> STEEL_OF_THE_FOUNDERS = ITEMS.register("steel_of_the_founders", ItemBase::new);
     public static final RegistryObject<Item> BIGBOYIUM = ITEMS.register("bigboyium", ItemBase::new);
+
+    // BLOCKS
+    //public static final RegistryObject<Block> BIGBOYIUM_ORE = BLOCKS.register("bigboyium_ore", BigboyiumOreBlock::new);
+    public static final RegistryObject<Block> BIGBOYIUM_ORE = BLOCKS.register("bigboyium_ore", () -> new Block(Block.Properties.create(Material.ROCK)));
+
+    // BLOCK ITEMS
+    public static final RegistryObject<Item> BIGBOYIUM_ORE_ITEM = ITEMS.register("bigboyium_ore", () -> new BlockItemBase(BIGBOYIUM_ORE.get()));
 }
